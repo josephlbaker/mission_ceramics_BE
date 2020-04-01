8633c5645993c8f1478fd1006c04695441812de1
josephlbaker <joelucasbaker@gmail.com>
2020-03-31 17:45:15 -0700

    Added payments route to accept card payments from FE

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const cors = require('cors');
 const express = require('express');
 const app = express();
 const SquareConnect = require('square-connect');
 const defaultClient = SquareConnect.ApiClient.instance;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
 app.use(cors());
 app.options('*', cors());
 
@@ -15,7 +20,9 @@ defaultClient.basePath = 'https://connect.squareupsandbox.com';
 const oauth2 = defaultClient.authentications['oauth2'];
 oauth2.accessToken = process.env.ACCESS_TOKEN;
 
-const apiInstance = new SquareConnect.CatalogApi();
+// RETRIEVING ITEMS IN CATALOG
+
+const catalog = new SquareConnect.CatalogApi();
 
 const opts = {
   'cursor': "",
@@ -23,12 +30,47 @@ const opts = {
 };
 
 app.get('/', (req, res) => {
-  apiInstance.listCatalog(opts).then(function (data) {
+  catalog.listCatalog(opts).then(function (data) {
     res.send(data);
   }, function (error) {
     console.error(error);
   });
 });
+
+app.post('/payments', async (req, res) => {
+  const request_params = req.body;
+
+  // length of idempotency_key should be less than 45
+  const idempotency_key = crypto.randomBytes(22).toString('hex');
+
+  // Charge the customer's card
+  const payments_api = new SquareConnect.PaymentsApi();
+  const request_body = {
+    source_id: request_params.nonce,
+    amount_money: {
+      amount: 100, // $1.00 charge
+      currency: 'USD'
+    },
+    idempotency_key: idempotency_key
+  };
+
+  try {
+    const response = await payments_api.createPayment(request_body);
+    res.status(200).json({
+      'title': 'Payment Successful',
+      'result': response
+    });
+  } catch (error) {
+    res.status(500).json({
+      'title': 'Payment Failure',
+      'result': error.response.text
+    });
+  }
+});
+
+
+
+
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!');
